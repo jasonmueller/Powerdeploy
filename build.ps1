7e52ed6095acce90884f6210d6fd6a35c35ffb39
Jason Mueller <jason@suspendedgravity.com>
2014-03-29 11:11:01 -0500

    Package Powerdeploy into zip file on build

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 properties {
   $buildFolder = Join-Path $PSScriptRoot '_build'
+  $packageFolder = Join-Path $PSScriptRoot '_package'
   $sourceFolder = Join-Path $PSScriptRoot 'Source'
   $version = git describe --tags --always --dirty
   $changeset = 'n/a'
@@ -7,11 +8,16 @@ properties {
 
 task default -depends Build
 task Build -depends Clean, Test, Package
-task Package -depends Version, Squirt, Unversion
+task Package -depends Version, Squirt, Unversion, Zip
+
+task Zip {
+    exec { ."$sourceFolder\Tools\7za.exe" a -r "$packageFolder\Powerdeploy-$version.zip" "$buildFolder\*" }
+}
 
 task Squirt {
     Copy-Item $sourceFolder\* $buildFolder -Recurse -Exclude .git
     Get-ChildItem $buildFolder *.Tests.ps1 -Recurse | Remove-Item
+    Get-ChildItem $buildFolder TestHelpers.ps1 | Remove-Item
 }
 
 task Test { 
@@ -42,6 +48,9 @@ task Unversion {
 task Clean { 
     if (Test-Path $buildFolder) {
         Remove-Item $buildFolder -Recurse -Force
+    }
+    if (Test-Path $packageFolder) {
+        Remove-Item $packageFolder -Recurse -Force
     }
     New-Item $buildFolder -ItemType Directory | Out-Null
 }
