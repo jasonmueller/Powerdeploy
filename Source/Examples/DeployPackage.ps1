454401e8af3f4d33b07c9780d0187cf337f34f3f
Jason Mueller <jason@suspendedgravity.com>
2014-08-03 23:05:23 -0500

    Include example script using variable to deploy

---
+++ Source/Examples/DeployPackage.ps1
@@ -0,0 +1,41 @@
+param (
+    [String]
+    [Parameter(Position = 0, Mandatory = $true)]
+    $PackageArchive,
+
+    [String]
+    [Parameter(Position = 1, Mandatory = $true)]
+    $EnvironmentName,
+
+    [String]
+    [Parameter(Position = 2, Mandatory = $true)]
+    $ComputerName,
+
+    [System.Uri]
+    [Parameter(Position = 3, Mandatory = $true)]
+    $SettingsPath
+)
+
+if (-not (Get-Module Powerdeploy)) {
+    $shouldUnload = $true
+
+    Import-Module "$PSScriptRoot\..\Powerdeploy"
+}
+
+$configurationVariables = Get-ConfigurationVariable -SettingsPath $SettingsPath
+$applicableVariables = $configurationVariables | `
+    Resolve-DeploymentVariable `
+        -EnvironmentName $EnvironmentName `
+        -ComputerName $ComputerName `
+        -AsHashTable
+
+Invoke-Powerdeploy `
+    -PackageArchive $PackageArchive `
+    -Environment $EnvironmentName `
+    -ComputerName $ComputerName `
+    -Variable $applicableVariables `
+    -Verbose
+
+if ($shouldUnload) {
+    Remove-Module Powerdeploy
+}
